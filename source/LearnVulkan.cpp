59a07f5846ef955c62c5c2948a12bc9f5d84ff0d
surin.i <surin.i@skbkontur.ru>
2025-02-22 15:40:30 +0500

    create window instance

--- source/LearnVulkan.cpp
+++ source/LearnVulkan.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 class HelloTriangleApplication 
 {
@@ -19,16 +20,18 @@ public:
 private:
     void initWindow()
     {
+        glfwInit();
+
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
         window = glfwCreateWindow(width, height, "LearnVulkan", nullptr, nullptr);
-
     }
 
-    void initVulkan() const
+    void initVulkan()
     {
-
+        createInstance();
+        CheckGlfwAvailableExtensions();
     }
 
     void mainLoop() const
@@ -41,13 +44,78 @@ private:
 
     void cleanup() const
     {
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
     }
 
 private:
+
+    void createInstance()
+    {
+        auto applicationInfo = CreateApplicationInfo();
+        auto createInfo = CreateInstanceCreateInfo(applicationInfo);
+        auto createInstanceResult = vkCreateInstance(&createInfo, nullptr, &instance);
+
+        if (createInstanceResult != VK_SUCCESS)
+        {
+            throw std::runtime_error("Failed create glfw instance");
+        }
+    }
+
+    static VkInstanceCreateInfo CreateInstanceCreateInfo(const VkApplicationInfo &applicationInfo)
+    {
+        VkInstanceCreateInfo createInfo{};
+        createInfo.pApplicationInfo = &applicationInfo;
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+    	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        return createInfo;
+    }
+
+	static VkApplicationInfo CreateApplicationInfo()
+    {
+        VkApplicationInfo applicationInfo{};
+        applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        applicationInfo.pApplicationName = "LearnVulkan";
+        applicationInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        applicationInfo.pEngineName = "No Engine";
+        applicationInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        applicationInfo.apiVersion = VK_API_VERSION_1_0;
+
+        return applicationInfo;
+    }
+
+    static void CheckGlfwAvailableExtensions()
+    {
+        auto extensions = GetExtensionsSupport();
+        uint32_t glfwExtensionCount = 0;
+        auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+    }
+
+    static std::vector<VkExtensionProperties> GetExtensionsSupport()
+    {   
+        uint32_t extensionsCount = 0;
+
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionsCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionsCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionsCount, extensions.data());
+
+        return extensions;
+    }
+
+private:
     GLFWwindow* window;
+    VkInstance instance;
 
     const uint32_t width = 800;
     const uint32_t height = 600;
